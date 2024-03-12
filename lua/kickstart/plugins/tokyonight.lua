847adc3fe612f12cae884c3126322cb2e68d2aa6
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2024-03-12 22:35:46 +0000

    Change config to init in tokyonight.lua

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -7,7 +7,7 @@ return {
     'folke/tokyonight.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
+    init = function()
       -- Load the colorscheme here
       vim.cmd.colorscheme 'tokyonight-night'
 
