fd998849e11cd757fe23ea78638ab0150b4d95ca
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2024-03-12 23:00:54 +0000

    Add kotlin and docker lsps

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -132,19 +132,18 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        gopls = {},
-        pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
+        gopls = {},
+        pyright = {},
+        -- rust_analyzer = {},
         tsserver = {},
-        --
-
+        kotlin_language_server = {},
+        dockerls = {},
+        docker_compose_language_service = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes { ...},
